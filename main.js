03ba069e7dd138e585612bfb1c1f32f5584cc43b
Eduarda <rochaeduarda98133@gmail.com>
2024-02-14 14:54:46 -0300

    terceiro commit

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-function addNewTask(){
-    var list = document.getElementById("list"); 
-    var text = document.getElementById("task_name").value;
+const button = document.querySelector(".task_button");
 
-if(text.length == 0){
+function addNewTask() {
+  let list = document.getElementById("list");
+  let text = document.getElementById("new_task").value;
+
+  if (text.length == 0) {
     alert('Não foi adicionada nenhuma tarefa.');
+  }
+
+  let listIten = document.createElement("li");
+  listIten.className = "list-iten";
+
+  let textElement = document.createTextNode(text);
+  listIten.appendChild(textElement);
+  list.appendChild(listIten);
 }
 
-    var listIten = document.createElement("li");
-    listIten.className = "list-iten";
+function deleteTask(){
     
-    const textElement = document.createTextNode(text);
-    listIten.appendChild(textElement);
-    list.appendChild(listIten);
-    }
+}
+button.addEventListener('click', addNewTask);
